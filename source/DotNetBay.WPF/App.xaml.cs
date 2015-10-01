37cbe613e7c88fb63186a645ac373f3d3170182c
Raphael Bolliger <raphi@raphibolliger.ch>
2015-10-01 12:27:17 +0200

    - first part of Lab01.WPF

---
+++ source/DotNetBay.WPF/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using DotNetBay.Core;
+using DotNetBay.Core.Execution;
+using DotNetBay.Data.FileStorage;
+using DotNetBay.Interfaces;
+using DotNetBay.Model;
+
+namespace DotNetBay.WPF
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static readonly IMainRepository MainRepository =
+            new FileSystemMainRepository(@"/DotNetBay.Files/data.json");
+
+        public static readonly IAuctionRunner AuctionRunner = new AuctionRunner(MainRepository);
+
+        public App()
+        {
+            var memberService = new SimpleMemberService(MainRepository);
+            var service = new AuctionService(MainRepository, memberService);
+
+            if (!service.GetAll().Any())
+            {
+                var me = memberService.GetCurrentMember();
+                service.Save(new Auction
+                {
+                    Title = "My First Auction",
+                    StartDateTimeUtc = DateTime.UtcNow.AddSeconds(10),
+                    EndDateTimeUtc = DateTime.UtcNow.AddDays(14),
+                    StartPrice = 72,
+                    Seller = me
+                });
+            }
+        }
+    }
+}
